1e0a03d1ed86e9d84c3b19c9575a6b0e58d62352
Paul S <paulskaf@yahoo.fr>
2021-05-04 18:54:18 +0200

    Esx Extraitem

---
+++ resources/[esx]/esx_extraitems/sqls/esx_extraitems_simammo.sql
@@ -0,0 +1,38 @@
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('darknet', 'Dark Net', 1, 0, 1),
+	('bandage', 'Bandage', 1, 0, 1),
+	('binoculars', 'Jumelle', 1, 0, 1),
+	('bulletproof', 'Gilet pare-balle', 1, 0, 1),
+	('defib', 'Defibrilateur', 1, 0, 1),
+	('drill', 'Drill', 1, 0, 1),
+	('firework', 'Feu d\'artifice', 1, 0, 1),
+	('firstaidkit', 'Trousse de premier soin', 1, 0, 1),
+	('handcuffs', 'Menotte', 1, 0, 1),
+	('handcuffkey', 'Clés de menottes', 1, 0, 1),
+	('lockpick', 'Kit de crochetage', 1, 0, 1),
+	('oxygenmask', 'Kit de plongée', 1, 0, 1),
+	('repairkit', 'Kit de réparation', 1, 0, 1),
+	('tirekit', 'Kit de pneu', 1, 0, 1),
+	('vehgps', 'GPS', 1, 0, 1),
+	('weakit', 'Kit d\'arme', 1, 0, 1)
+;
+
+INSERT INTO `shops` (store, item, price) VALUES
+	('TwentyFourSeven', 'darknet', 25),
+	('TwentyFourSeven', 'bandage', 200),
+	('TwentyFourSeven', 'binoculars', 50),
+	('TwentyFourSeven', 'bulletproof', 400),
+	('TwentyFourSeven', 'defib', 1200),
+	('TwentyFourSeven', 'drill', 180),
+	('TwentyFourSeven', 'firework', 15),
+	('TwentyFourSeven', 'firstaidkit', 600),
+	('TwentyFourSeven', 'handcuffs', 400),
+	('TwentyFourSeven', 'handcuffkey', 50),
+	('TwentyFourSeven', 'lockpick', 100),
+	('TwentyFourSeven', 'oxygenmask', 400),
+	('TwentyFourSeven', 'repairkit', 150),
+	('TwentyFourSeven', 'tirekit', 25),
+	('TwentyFourSeven', 'vehgps', 50),
+	('TwentyFourSeven', 'weakit', 800)
+;
