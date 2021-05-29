40d9fd0310aedbd921be3b3565bb372684698694
Paul S <paulskaf@yahoo.fr>
2021-05-29 14:05:52 +0200

    Fix SQL Query for esx shop

--- resources/[esx]/esx_shops/server/main.lua
+++ resources/[esx]/esx_shops/server/main.lua
@@ -11,15 +11,12 @@ MySQL.ready(function()
 					ShopItems[shopResult[i].store] = {}
 				end
 
-				if shopResult[i].limit == -1 then
-					shopResult[i].limit = 30
-				end
+				
 
 				table.insert(ShopItems[shopResult[i].store], {
 					label = shopResult[i].label,
 					item  = shopResult[i].item,
 					price = shopResult[i].price,
-					limit = shopResult[i].limit
 				})
 			else
 				print(('esx_shops: invalid item "%s" found!'):format(shopResult[i].item))
@@ -36,7 +33,6 @@ RegisterServerEvent('esx_shops:buyItem')
 AddEventHandler('esx_shops:buyItem', function(itemName, amount, zone)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	local sourceItem = xPlayer.getInventoryItem(itemName)
 
 	amount = ESX.Math.Round(amount)
 
@@ -63,9 +59,7 @@ AddEventHandler('esx_shops:buyItem', function(itemName, amount, zone)
 	-- can the player afford this item?
 	if xPlayer.getMoney() >= price then
 		-- can the player carry the said amount of x item?
-		if sourceItem.limit ~= -1 and (sourceItem.count + amount) > sourceItem.limit then
-			TriggerClientEvent('esx:showNotification', _source, _U('player_cannot_hold'))
-		else
+		if xPlayer.canCarryItem(itemname,amount) then
 			xPlayer.removeMoney(price)
 			xPlayer.addInventoryItem(itemName, amount)
 			TriggerClientEvent('esx:showNotification', _source, _U('bought', amount, itemLabel, ESX.Math.GroupDigits(price)))
